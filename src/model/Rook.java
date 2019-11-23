75185dc5d7ce15063cf28f24a98200f56ee23af2
branmkam <54331071+branmkam@users.noreply.github.com>
2019-11-23 14:26:30 -0500

    new increment methods added

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -16,7 +16,48 @@ public class Rook extends ChessPiece {
 
     @Override
     public ArrayList<Move> getAllPossibleMoves() {
-        return null; // TODO add this
+        ArrayList<Move> possibleMoves = new ArrayList<Move>();
+        possibleMoves.addAll(getAllMovesInDirection(1,0));
+        possibleMoves.addAll(getAllMovesInDirection(0,1));
+        possibleMoves.addAll(getAllMovesInDirection(-1,0));
+        possibleMoves.addAll(getAllMovesInDirection(0,-1));
+        return possibleMoves;
+    }
+
+
+    public ArrayList<Move> getAllMovesInDirection(int dx, int dy)
+    {
+      ArrayList<Move> output = new ArrayList<Move>();
+      cx = this.getX();
+      cy = this.getY();
+      for (int x = 0; x < 8; x++)
+      {
+        cx += dx;
+        cy += dy;
+        if(cx >= 8 || cy >= 8 || cx < 0 || cy < 0)
+        {
+          break;
+        }
+        if(!board.getSpotAt(cx, cy).isEmpty())
+        {
+          if(board.getSpotAt(cx, cy).getPieceColor().equals(this.color))
+          {
+            break;
+          }
+          else
+          {
+            output.add(new Move(this, this.getX(), this.getY(), cx, cy));
+            break;
+          }
+        }
+        else
+        {
+          output.add(new Move(this, this.getX(), this.getY(), cx, cy));
+          continue;
+        }
+      }
+
+      return output;
     }
 
     public int getNumPoints() {
