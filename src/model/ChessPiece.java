ed670993a580ea59b2d41355c3bf3fb2477e38fc
leifr-unc <leifr@ad.unc.edu>
2019-11-23 11:21:00 -0500

    Added basic methods to chessPiece

--- src/model/ChessPiece.java
+++ src/model/ChessPiece.java
@@ -1,5 +1,11 @@
 package model;
 
 public abstract class ChessPiece {
+    private int x;
+    private int y;
+
+    public abstract boolean getCanMoveToPosition(int x, int y);
+    public abstract int getNumPoints();
+    public abstract String getName(); // "King", "Queen", "Pawn", etc.
 
 }
