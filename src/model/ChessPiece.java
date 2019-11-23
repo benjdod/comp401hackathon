b13eb0910362bae966ea4bb8f10b971a2943fd49
branmkam <54331071+branmkam@users.noreply.github.com>
2019-11-23 11:44:46 -0500

    added chesspiece player enum

--- src/model/ChessPiece.java
+++ src/model/ChessPiece.java
@@ -3,9 +3,36 @@ package model;
 public abstract class ChessPiece {
     private int x;
     private int y;
+    private Player p;
+
+    private ChessPiece(Player player)
+    {
+      if (player == Player.WHITE)
+      {
+        p = Player.WHITE;
+      }
+      else
+      {
+        p = Player.BLACK;
+      }
+    }
 
     public abstract boolean getCanMoveToPosition(int x, int y);
     public abstract int getNumPoints();
     public abstract String getName(); // "King", "Queen", "Pawn", etc.
-
-}
+    public abstract String getPieceColor()
+    {
+      if p == Player.WHITE()
+      {
+        return "white";
+      }
+      else
+      {
+        return "black";
+      }
+    }
+    public abstract Player getPlayer()
+    {
+      return p;
+    }
+  }
