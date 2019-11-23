a662cc5c62cbb3031da7fe5104e28303957c7daa
branmkam <54331071+branmkam@users.noreply.github.com>
2019-11-23 12:46:18 -0500

    added AI player

---
+++ src/main/AIPlayer.java
@@ -0,0 +1,43 @@
+package main;
+import java.util.Random;
+
+public class AIPlayer implements Player {
+
+  private Player.Color _color;
+
+  public AIPlayer(Color color)
+  {
+    this._color = color;
+  }
+
+  public Move checkForMoves(ChessBoard cb, int x, int y)
+  {
+    ArrayList<Move> eligibleMoves = new ArrayList<Move>();
+    for(ChessSpot s : cb)
+    {
+      if(s.getPiece() != null && s.getPieceColor().equals(_color))
+      {
+        ChessPiece p = s.getPiece();
+        for(ChessSpot s : cb)
+        {
+          if(p.getCanMoveToPosition(x, y))
+          {
+            eligibleMoves.add(new Move(this, s.getPiece().getX(), s.getPiece().getY(), x, y;
+          }
+        }
+      }
+    }
+
+    Random r = new Random();
+    int moveToSelect = r.nextInt(0, eligibleMoves.size()-1);
+
+    if(eligibleMoves.size > 0)
+    {
+      return eligibleMoves.get(moveToSelect);
+    }
+    else
+    {
+      return null;
+    }
+  }
+}
