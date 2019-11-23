4ff74ff044ad57f0c0e83222200a228017e8bb77
branmkam <54331071+branmkam@users.noreply.github.com>
2019-11-23 13:15:01 -0500

    got knight possiblemoves check

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -9,8 +9,41 @@ public class Knight extends ChessPiece {
     }
 
     public boolean getCanMoveToPosition(int x, int y) {
-        return ((Math.abs(this.getX() - x) == 2 && Math.abs(this.getY() - y) == 1)
-        || (Math.abs(this.getX() - x) == 1 && Math.abs(this.getY() - y) == 2)) ? true : false;
+        ArrayList<Move> possibleMoves = getAllPossibleMoves(cb);
+        Move moveCheck = new Move(this.getPlayer(), this.getX(), this.getY(), x, y);
+        boolean isPossibleMove = false;
+        for(int i = 0; i < possibleMoves.size(); i++)
+        {
+          if(possibleMoves.get(i).equals(moveCheck))
+          {
+            isPossibleMove = true;
+          }
+        }
+
+        return isPossibleMove;
+    }
+
+    public ArrayList<Move> getAllPossibleMoves(ChessBoard cb)
+    {
+      ArrayList <Move> allPossibleMoves = new ArrayList<Move>();
+      for(ChessSpot s: cb)
+      {
+        boolean isKnightMove = false;
+        if((Math.abs(this.getX() - x) == 2 && Math.abs(this.getY() - y) == 1)
+        || (Math.abs(this.getX() - x) == 1 && Math.abs(this.getY() - y) == 2))
+        {
+          isKnightMove = true;
+        }
+
+        if(isKnightMove)
+        {
+          if s.isEmpty()
+          {
+            allPossibleMoves.add(new Move(this.getPlayer(), this.getX(), this.getY(), s.getSpotX(), s.getSpotY()));
+          }
+        }
+      }
+      return allPossibleMoves;
     }
 
     public int getNumPoints() {
